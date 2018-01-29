f3868c3cf06074aa62e50624ae69496b8bc95944
Angus McLauchlan <angus@itsolver.net>
2018-01-29 23:55:52 +1000

    Add firebase for deployment in Gulp

---
+++ gulp/tasks/firebase.js
@@ -0,0 +1,23 @@
+'use strict';
+var fs    = require('fs');
+var gulp  = require('gulp');
+const shell = require('gulp-shell')
+
+// include paths file
+var paths = require('../paths');
+
+// 'gulp firebase' -- reads rsync credentials file and incrementally uploads site to server
+gulp.task('firebase', shell.task([
+  'firebase deploy'
+]))
+
+// 'gulp submit:sitemap` -- submit sitemap XML file to Google and Bing
+gulp.task('submit:sitemap', (cb) => {
+  var SitemapUrl = paths.prodUrl + '/sitemap.xml';
+
+  require('submit-sitemap').submitSitemap(SitemapUrl, function(err) {
+    if (err)
+      console.warn(err);
+      cb();
+  });
+});
