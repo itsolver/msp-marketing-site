7bacc6cb91e4a5175548a3af7aaa690114c1b2f4
Angus McLauchlan <angus@itsolver.net>
2018-01-31 15:00:04 +1000

    Local build speed up

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -19,7 +19,7 @@ gulp.task('site:tmp', () => {
 gulp.task('site', done => {
   if (!argv.prod) {
     done();
-    shell.exec('bundle exec jekyll build --config _config.yml,_config.dev.yml'); // To speed up site build for local testing, add --limit_posts 1
+    shell.exec('bundle exec jekyll build --config _config.yml,_config.dev.yml --limit_posts 5'); // To speed up site build for local testing, add --limit_posts 1
   } else if (argv.prod) {
     shell.exec('bundle exec jekyll build');
     done();
