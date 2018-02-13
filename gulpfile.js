6559095ac94816287800cf65c9733314d61ef938
Angus McLauchlan <angus@itsolver.net>
2018-02-13 13:33:28 +1000

    Critical css all html

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('build', gulp.series('clean', 'assets', 'build:site', 'html', 'xml'));
 // 'gulp critical' -- builds critical path CSS includes
 //   WARNING: run this after substantial CSS changes
 //   WARNING: .html files referenced need to exist, run after `gulp build` to ensure.
-gulp.task('critical', gulp.series('styles:critical:home', 'styles:critical:archive')); //'styles:critical:post'));
+gulp.task('critical', gulp.series('styles:critical:home', 'styles:critical:archive', 'styles:critical:post'));
 
 // 'gulp rebuild' -- WARNING: removes all assets, images, and built site
 gulp.task('rebuild', gulp.series('clean', 'clean:images'));
