3900c1e36284049e23360805280e26a7d362f169
Angus McLauchlan <angus@itsolver.net>
2018-02-22 22:43:02 +1000

    Update service worker version

--- src/sw.js
+++ src/sw.js
@@ -9,7 +9,7 @@ layout: null
  * Register service worker.
  * ========================================================== */
 
-const PRECACHE = 'precache-v5';
+const PRECACHE = 'precache-v6';
 const RUNTIME = 'runtime';
 const HOSTNAME_WHITELIST = [
   "itsolver.net"
