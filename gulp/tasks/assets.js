62fc5233ca18036894c3979f820866a275190b73
Angus McLauchlan <angus@itsolver.net>
2018-02-22 20:15:34 +1000

    Simplify legal sub-nav, removed js

--- gulp/tasks/assets.js
+++ gulp/tasks/assets.js
@@ -38,7 +38,6 @@ gulp.task('scripts', () => {
   // top to bottom, so you want vendor scripts etc on top
   return gulp.src([
     paths.jsFiles + '/lozad.min.js',
-    paths.jsFiles + '/ac-localnav.built.min.js'
   ])
     .pipe(newer(paths.jsFilesTemp + '/index.js', { dest: paths.jsFilesTemp, ext: '.js' }))
     .pipe(when(!argv.prod, sourcemaps.init()))
