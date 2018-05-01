9e3e00184b1862e1d21ebc0dd4e3827d32d64457
itsolver <26458299+itsolver@users.noreply.github.com>
2018-05-01 15:20:01 +1000

    Adding in stripe-payments-demo, part 1

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+const config = require('config');
+const functions = require('firebase-functions');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = frequire('body-parser');
+const path = require('path');
+const router = express.Router();
+const stripe = require('stripe')(config.stripe.secretKey);
+stripe.setApiVersion(config.stripe.apiVersion);
+
+const app = express();
+app.get('/buy/on-demand', (request, response) => {
+  res.render('index.html');
+});
+
+exports.app = functions.https.onRequest(app);
