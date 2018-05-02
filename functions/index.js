5785c6a3c9c5dfe40237877be91cb73d80851ef6
itsolver <26458299+itsolver@users.noreply.github.com>
2018-05-02 15:01:44 +1000

    Troubleshooting firebase functions...

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,16 @@
+'use strict';
+
 const config = require('./config');
 const functions = require('firebase-functions');
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
-const router = express.Router();
+const {orders, products} = require('./orders');
 const stripe = require('stripe')(config.stripe.secretKey);
 stripe.setApiVersion(config.stripe.apiVersion);
 
+const router = express.Router();
 const app = express();
 app.get('/buy/on-demand', (request, response) => {
   res.render('/buy/on-demand');
--- functions/routes.js
+++ functions/routes.js
@@ -10,15 +10,14 @@
 // 'use strict';
 
 // const config = require('./config');
-// const setup = require('./setup');
-// //const {orders, products} = require('/orders');
+// const {orders, products} = require('./orders');
 // const express = require('express');
-// const app = express();
+// const router = express.Router();
 // const stripe = require('stripe')(config.stripe.secretKey);
 // stripe.setApiVersion(config.stripe.apiVersion);
 
-// // Render the main app HTML.
-// // app.get('/', (req, res) => {
+// // // Render the main app HTML.
+// // router.get('/', (req, res) => {
 // //   res.render('index.html');
 // // });
 
@@ -35,7 +34,7 @@
 //  */
 
 // // Create an order on the backend.
-// app.post('/orders', async (req, res, next) => {
+// router.post('/orders', async (req, res, next) => {
 //   let {currency, items, email, shipping} = req.body;
 //   try {
 //     let order = await orders.create(currency, items, email, shipping);
@@ -45,245 +44,246 @@
 //   }
 // });
 
-// // // Complete payment for an order using a source.
-// // app.post('/orders/:id/pay', async (req, res, next) => {
-// //   let {source} = req.body;
-// //   try {
-// //     // Retrieve the order associated to the ID.
-// //     let order = await orders.retrieve(req.params.id);
-// //     // Verify that this order actually needs to be paid.
-// //     if (
-// //       order.metadata.status === 'pending' ||
-// //       order.metadata.status === 'paid'
-// //     ) {
-// //       return res.status(403).json({order, source});
-// //     }
-// //     // Dynamically evaluate if 3D Secure should be used.
-// //     if (source && source.type === 'card') {
-// //       // A 3D Secure source may be created referencing the card source.
-// //       source = await dynamic3DS(source, order, req);
-// //     }
-// //     // Demo: In test mode, replace the source with a test token so charges can work.
-// //     if (!source.livemode) {
-// //       source.id = 'tok_visa';
-// //     }
-// //     // Pay the order using the Stripe source.
-// //     if (source && source.status === 'chargeable') {
-// //       let charge, status;
-// //       try {
-// //         charge = await stripe.charges.create(
-// //           {
-// //             source: source.id,
-// //             amount: order.amount,
-// //             currency: order.currency,
-// //             receipt_email: order.email,
-// //           },
-// //           {
-// //             // Set a unique idempotency key based on the order ID.
-// //             // This is to avoid any race conditions with your webhook handler.
-// //             idempotency_key: order.id,
-// //           }
-// //         );
-// //       } catch (err) {
-// //         // This is where you handle declines and errors.
-// //         // For the demo we simply set to failed.
-// //         status = 'failed';
-// //       }
-// //       if (charge && charge.status === 'succeeded') {
-// //         status = 'paid';
-// //       } else if (charge) {
-// //         status = charge.status;
-// //       } else {
-// //         status = 'failed';
-// //       }
-// //       // Update the order with the charge status.
-// //       order = await orders.update(order.id, {metadata: {status}});
-// //     }
-// //     return res.status(200).json({order, source});
-// //   } catch (err) {
-// //     return res.status(500).json({error: err.message});
-// //   }
-// // });
+// // Complete payment for an order using a source.
+// router.post('/orders/:id/pay', async (req, res, next) => {
+//   let {source} = req.body;
+//   try {
+//     // Retrieve the order associated to the ID.
+//     let order = await orders.retrieve(req.params.id);
+//     // Verify that this order actually needs to be paid.
+//     if (
+//       order.metadata.status === 'pending' ||
+//       order.metadata.status === 'paid'
+//     ) {
+//       return res.status(403).json({order, source});
+//     }
+//     // Dynamically evaluate if 3D Secure should be used.
+//     if (source && source.type === 'card') {
+//       // A 3D Secure source may be created referencing the card source.
+//       source = await dynamic3DS(source, order, req);
+//     }
+//     // Demo: In test mode, replace the source with a test token so charges can work.
+//     if (!source.livemode) {
+//       source.id = 'tok_visa';
+//     }
+//     // Pay the order using the Stripe source.
+//     if (source && source.status === 'chargeable') {
+//       let charge, status;
+//       try {
+//         charge = await stripe.charges.create(
+//           {
+//             source: source.id,
+//             amount: order.amount,
+//             currency: order.currency,
+//             receipt_email: order.email,
+//           },
+//           {
+//             // Set a unique idempotency key based on the order ID.
+//             // This is to avoid any race conditions with your webhook handler.
+//             idempotency_key: order.id,
+//           }
+//         );
+//       } catch (err) {
+//         // This is where you handle declines and errors.
+//         // For the demo we simply set to failed.
+//         status = 'failed';
+//       }
+//       if (charge && charge.status === 'succeeded') {
+//         status = 'paid';
+//       } else if (charge) {
+//         status = charge.status;
+//       } else {
+//         status = 'failed';
+//       }
+//       // Update the order with the charge status.
+//       order = await orders.update(order.id, {metadata: {status}});
+//     }
+//     return res.status(200).json({order, source});
+//   } catch (err) {
+//     return res.status(500).json({error: err.message});
+//   }
+// });
 
-// // // Webhook handler to process payments for sources asynchronously.
-// // app.post('/webhook', async (req, res) => {
-// //   let data;
-// //   // Check if webhook signing is configured.
-// //   if (config.stripe.webhookSecret) {
-// //     // Retrieve the event by verifying the signature using the raw body and secret.
-// //     let event;
-// //     let signature = req.headers['stripe-signature'];
-// //     try {
-// //       event = stripe.webhooks.constructEvent(
-// //         req.rawBody,
-// //         signature,
-// //         config.stripe.webhookSecret
-// //       );
-// //     } catch (err) {
-// //       console.log(`⚠️  Webhook signature verification failed.`);
-// //       return res.sendStatus(400);
-// //     }
-// //     // Extract the object from the event.
-// //     data = event.data;
-// //   } else {
-// //     // Webhook signing is recommended, but if the secret is not configured in `config.js`,
-// //     // retrieve the event data directly from the request body.
-// //     data = req.body.data;
-// //   }
-// //   const object = data.object;
+// // Webhook handler to process payments for sources asynchronously.
+// router.post('/webhook', async (req, res) => {
+//   let data;
+//   // Check if webhook signing is configured.
+//   if (config.stripe.webhookSecret) {
+//     // Retrieve the event by verifying the signature using the raw body and secret.
+//     let event;
+//     let signature = req.headers['stripe-signature'];
+//     try {
+//       event = stripe.webhooks.constructEvent(
+//         req.rawBody,
+//         signature,
+//         config.stripe.webhookSecret
+//       );
+//     } catch (err) {
+//       console.log(`⚠️  Webhook signature verification failed.`);
+//       return res.sendStatus(400);
+//     }
+//     // Extract the object from the event.
+//     data = event.data;
+//   } else {
+//     // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+//     // retrieve the event data directly from the request body.
+//     data = req.body.data;
+//   }
+//   const object = data.object;
 
-// //   // Monitor `source.chargeable` events.
-// //   if (
-// //     object.object === 'source' &&
-// //     object.status === 'chargeable' &&
-// //     object.metadata.order
-// //   ) {
-// //     const source = object;
-// //     console.log(`🔔  Webhook received! The source ${source.id} is chargeable.`);
-// //     // Find the corresponding order this source is for by looking in its metadata.
-// //     const order = await orders.retrieve(source.metadata.order);
-// //     // Verify that this order actually needs to be paid.
-// //     if (
-// //       order.metadata.status === 'pending' ||
-// //       order.metadata.status === 'paid' ||
-// //       order.metadata.status === 'failed'
-// //     ) {
-// //       return res.sendStatus(403);
-// //     }
+//   // Monitor `source.chargeable` events.
+//   if (
+//     object.object === 'source' &&
+//     object.status === 'chargeable' &&
+//     object.metadata.order
+//   ) {
+//     const source = object;
+//     console.log(`🔔  Webhook received! The source ${source.id} is chargeable.`);
+//     // Find the corresponding order this source is for by looking in its metadata.
+//     const order = await orders.retrieve(source.metadata.order);
+//     // Verify that this order actually needs to be paid.
+//     if (
+//       order.metadata.status === 'pending' ||
+//       order.metadata.status === 'paid' ||
+//       order.metadata.status === 'failed'
+//     ) {
+//       return res.sendStatus(403);
+//     }
 
-// //     // Note: We're setting an idempotency key below on the charge creation to
-// //     // prevent any race conditions. It's set to the order ID, which protects us from
-// //     // 2 different sources becoming `chargeable` simultaneously for the same order ID.
-// //     // Depending on your use cases and your idempotency keys, you might need an extra
-// //     // lock surrounding your webhook code to prevent other race conditions.
-// //     // Read more on Stripe's best practices here for asynchronous charge creation:
-// //     // https://stripe.com/docs/sources/best-practices#charge-creation
+//     // Note: We're setting an idempotency key below on the charge creation to
+//     // prevent any race conditions. It's set to the order ID, which protects us from
+//     // 2 different sources becoming `chargeable` simultaneously for the same order ID.
+//     // Depending on your use cases and your idempotency keys, you might need an extra
+//     // lock surrounding your webhook code to prevent other race conditions.
+//     // Read more on Stripe's best practices here for asynchronous charge creation:
+//     // https://stripe.com/docs/sources/best-practices#charge-creation
 
-// //     // Pay the order using the source we just received.
-// //     let charge, status;
-// //     try {
-// //       charge = await stripe.charges.create(
-// //         {
-// //           source: source.id,
-// //           amount: order.amount,
-// //           currency: order.currency,
-// //           receipt_email: order.email,
-// //         },
-// //         {
-// //           // Set a unique idempotency key based on the order ID.
-// //           // This is to avoid any race conditions with your webhook handler.
-// //           idempotency_key: order.id,
-// //         }
-// //       );
-// //     } catch (err) {
-// //       // This is where you handle declines and errors.
-// //       // For the demo, we simply set the status to mark the order as failed.
-// //       status = 'failed';
-// //     }
-// //     if (charge && charge.status === 'succeeded') {
-// //       status = 'paid';
-// //     } else if (charge) {
-// //       status = charge.status;
-// //     } else {
-// //       status = 'failed';
-// //     }
-// //     // Update the order status based on the charge status.
-// //     await orders.update(order.id, {metadata: {status}});
-// //   }
+//     // Pay the order using the source we just received.
+//     let charge, status;
+//     try {
+//       charge = await stripe.charges.create(
+//         {
+//           source: source.id,
+//           amount: order.amount,
+//           currency: order.currency,
+//           receipt_email: order.email,
+//         },
+//         {
+//           // Set a unique idempotency key based on the order ID.
+//           // This is to avoid any race conditions with your webhook handler.
+//           idempotency_key: order.id,
+//         }
+//       );
+//     } catch (err) {
+//       // This is where you handle declines and errors.
+//       // For the demo, we simply set the status to mark the order as failed.
+//       status = 'failed';
+//     }
+//     if (charge && charge.status === 'succeeded') {
+//       status = 'paid';
+//     } else if (charge) {
+//       status = charge.status;
+//     } else {
+//       status = 'failed';
+//     }
+//     // Update the order status based on the charge status.
+//     await orders.update(order.id, {metadata: {status}});
+//   }
 
-// //   // Monitor `charge.succeeded` events.
-// //   if (
-// //     object.object === 'charge' &&
-// //     object.status === 'succeeded' &&
-// //     object.source.metadata.order
-// //   ) {
-// //     const charge = object;
-// //     console.log(`🔔  Webhook received! The charge ${charge.id} succeeded.`);
-// //     // Find the corresponding order this source is for by looking in its metadata.
-// //     const order = await orders.retrieve(charge.source.metadata.order);
-// //     // Update the order status to mark it as paid.
-// //     await orders.update(order.id, {metadata: {status: 'paid'}});
-// //   }
+//   // Monitor `charge.succeeded` events.
+//   if (
+//     object.object === 'charge' &&
+//     object.status === 'succeeded' &&
+//     object.source.metadata.order
+//   ) {
+//     const charge = object;
+//     console.log(`🔔  Webhook received! The charge ${charge.id} succeeded.`);
+//     // Find the corresponding order this source is for by looking in its metadata.
+//     const order = await orders.retrieve(charge.source.metadata.order);
+//     // Update the order status to mark it as paid.
+//     await orders.update(order.id, {metadata: {status: 'paid'}});
+//   }
 
-// //   // Monitor `source.failed`, `source.canceled`, and `charge.failed` events.
-// //   if (
-// //     (object.object === 'source' || object.object === 'charge') &&
-// //     (object.status === 'failed' || object.status === 'canceled')
-// //   ) {
-// //     const source = object.source ? object.source : object;
-// //     console.log(`🔔  Webhook received! Failure for ${object.id}.`);
-// //     if (source.metadata.order) {
-// //       // Find the corresponding order this source is for by looking in its metadata.
-// //       const order = await orders.retrieve(source.metadata.order);
-// //       // Update the order status to mark it as failed.
-// //       await orders.update(order.id, {metadata: {status: 'failed'}});
-// //     }
-// //   }
+//   // Monitor `source.failed`, `source.canceled`, and `charge.failed` events.
+//   if (
+//     (object.object === 'source' || object.object === 'charge') &&
+//     (object.status === 'failed' || object.status === 'canceled')
+//   ) {
+//     const source = object.source ? object.source : object;
+//     console.log(`🔔  Webhook received! Failure for ${object.id}.`);
+//     if (source.metadata.order) {
+//       // Find the corresponding order this source is for by looking in its metadata.
+//       const order = await orders.retrieve(source.metadata.order);
+//       // Update the order status to mark it as failed.
+//       await orders.update(order.id, {metadata: {status: 'failed'}});
+//     }
+//   }
 
-// //   // Return a 200 success code to Stripe.
-// //   res.sendStatus(200);
-// // });
+//   // Return a 200 success code to Stripe.
+//   res.sendStatus(200);
+// });
 
-// // // Dynamically create a 3D Secure source.
-// // const dynamic3DS = async (source, order, req) => {
-// //   // Check if 3D Secure is required, or trigger it based on a custom rule (in this case, if the amount is above a threshold).
-// //   if (source.card.three_d_secure === 'required' || order.amount > 5000) {
-// //     source = await stripe.sources.create({
-// //       amount: order.amount,
-// //       currency: order.currency,
-// //       type: 'three_d_secure',
-// //       three_d_secure: {
-// //         card: source.id,
-// //       },
-// //       metadata: {
-// //         order: order.id,
-// //       },
-// //       redirect: {
-// //         return_url: req.headers.origin,
-// //       },
-// //     });
-// //   }
-// //   return source;
-// // };
+// // Dynamically create a 3D Secure source.
+// const dynamic3DS = async (source, order, req) => {
+//   // Check if 3D Secure is required, or trigger it based on a custom rule (in this case, if the amount is above a threshold).
+//   if (source.card.three_d_secure === 'required' || order.amount > 5000) {
+//     source = await stripe.sources.create({
+//       amount: order.amount,
+//       currency: order.currency,
+//       type: 'three_d_secure',
+//       three_d_secure: {
+//         card: source.id,
+//       },
+//       metadata: {
+//         order: order.id,
+//       },
+//       redirect: {
+//         return_url: req.headers.origin,
+//       },
+//     });
+//   }
+//   return source;
+// };
 
-// // /**
-// //  * Routes exposing the config as well as the ability to retrieve products and orders.
-// //  */
+// /**
+//  * Routes exposing the config as well as the ability to retrieve products and orders.
+//  */
 
-// // // Expose the Stripe publishable key and other pieces of config via an endpoint.
-// // app.get('/config', (req, res) => {
-// //   res.json({
-// //     stripePublishableKey: config.stripe.publishableKey,
-// //     stripeCountry: config.stripe.country,
-// //     country: config.country,
-// //     currency: config.currency,
-// //   });
-// // });
+// // Expose the Stripe publishable key and other pieces of config via an endpoint.
+// router.get('/config', (req, res) => {
+//   res.json({
+//     stripePublishableKey: config.stripe.publishableKey,
+//     stripeCountry: config.stripe.country,
+//     country: config.country,
+//     currency: config.currency,
+//   });
+// });
 
-// // // Retrieve an order.
-// // app.get('/orders/:id', async (req, res) => {
-// //   try {
-// //     return res.status(200).json(await orders.retrieve(req.params.id));
-// //   } catch (err) {
-// //     return res.sendStatus(404);
-// //   }
-// // });
+// // Retrieve an order.
+// router.get('/orders/:id', async (req, res) => {
+//   try {
+//     return res.status(200).json(await orders.retrieve(req.params.id));
+//   } catch (err) {
+//     return res.sendStatus(404);
+//   }
+// });
 
 // // Retrieve all products.
-// app.get('/products', async (req, res) => {
+// router.get('/products', async (req, res) => {
 //   const productList = await products.list();
 //   // Check if products exist on Stripe Account.
 //   if (products.exist(productList)) {
 //     res.json(productList);
 //   } else {
 //     // We need to set up the products.
-//     console.log(`⚠️  We need to set up the products..`);
+//     await setup.run();
+//     res.json(await products.list());
 //   }
 // });
 
-// // // Retrieve a product by ID.
-// // app.get('/products/:id', async (req, res) => {
-// //   res.json(await products.retrieve(req.params.id));
-// // });
+// // Retrieve a product by ID.
+// router.get('/products/:id', async (req, res) => {
+//   res.json(await products.retrieve(req.params.id));
+// });
 
-// module.exports = app;
+// module.exports = router;
