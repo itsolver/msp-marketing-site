88683f724fec8b1309c5da413bd748f72e384a69
itsolver <26458299+itsolver@users.noreply.github.com>
2018-05-08 13:18:44 +1000

    Remove non-working stripe payments from master

--- functions/src/app/app.module.ts
+++
@@ -1,7 +0,0 @@
-declare module 'morgan';
-declare module 'stripe';
-declare module 'express';
-declare module 'cors';
-declare module 'firebase-functions';
-declare module 'path';
-declare module 'body-parser';
