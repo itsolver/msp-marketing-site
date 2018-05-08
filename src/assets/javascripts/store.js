4f6e92236589365439b4235d85557e3051ea993d
itsolver <26458299+itsolver@users.noreply.github.com>
2018-05-09 00:47:41 +1000

    Fix image path

--- src/assets/javascripts/store.js
+++ src/assets/javascripts/store.js
@@ -154,7 +154,7 @@ class Store {
       let lineItem = document.createElement('div');
       lineItem.classList.add('line-item');
       lineItem.innerHTML = `
-        <img class="image" src="/images/products/${product.id}.png">
+        <img class="image" src="/assets/images/products/${product.id}.png">
         <div class="label">
           <p class="product">${product.name}</p>
           <p class="sku">${Object.values(sku.attributes).join(' ')}</p>
